0588bc3e1920e53643717510fd46c0bfa35d94b6
jotase <jota.segovia@gmail.com>
2016-11-05 14:28:55 -0300

    section one completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = process.env.YOUTUBE_KEY;
 
 // Create a new component
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
