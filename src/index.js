2d856794167aabaaaf22ef16cbaefa8cf6df8736
jotase <jota.segovia@gmail.com>
2016-11-16 22:46:43 -0300

    2-32

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyDQrZZM702sWd-DoRez_KLlPo5GMab8Mmo';
 
-const API_KEY = process.env.YOUTUBE_KEY;
+class App extends Component {
+  constructor(props){
+    super(props);
 
-// Create a new component
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>;
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfing');
+  }
+
+  videoSearch(term) {
+    YTSearch({
+      key: API_KEY,
+      term: term},
+      (videos) => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+      });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
